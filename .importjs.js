ddbe66ccd5a6ab4a1ef1a2497bc30535e3bdcea5
Egill <egill@egill.xyz>
2022-05-20 17:16:47 +0000

    no message

---
+++ .importjs.js
@@ -0,0 +1,7 @@
+module.exports = {
+  useRelativePaths: false,
+  declarationKeyword: "import",
+  environments: ["browser", "node", "jest"],
+  emptyLineBetweenGroups: false,
+  danglingCommas: false,
+};
