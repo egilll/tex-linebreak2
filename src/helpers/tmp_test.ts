0df6298ba1c42be2b82da1987a0a503df99e4c05
Egill <egill@egill.xyz>
2022-05-22 16:18:20 +0000

    no message

--- src/helpers/tmp_test.ts
+++ src/helpers/tmp_test.ts
@@ -3,16 +3,19 @@ import {
   UnicodeLineBreakingClasses,
   convertEnumValuesOfLineBreakingPackageToUnicodeNames,
 } from 'src/typings/unicodeLineBreakingClasses';
+import { MIN_COST } from 'src/breakLines';
 
 const NON_BREAKING_SPACE = '\xa0';
 const SOFT_HYPHEN = '\u00AD';
 
 export enum PenaltyClasses {
-  MandatoryBreak,
-  GoodBreak,
-  OKBreak,
-  BadBreak,
-  VeryBadBreak,
+  MandatoryBreak = MIN_COST,
+  /** Spaces */
+  VeryGoodBreak = 0,
+  GoodBreak = 100,
+  OKBreak = 200,
+  BadBreak = 300,
+  VeryBadBreak = 600,
 }
 
 // story.match(/\p{General_Category=Letter}/gu);
@@ -28,45 +31,80 @@ export const getBreakablePoints = (input: string) => {
     /**
      * This contains the final space character, if any.
      */
-    let word = input.slice(breakPoints[i - 1]?.position || 0, breakPoints[i].position);
+    const word = input.slice(breakPoints[i - 1]?.position || 0, breakPoints[i].position);
+    const isLastSegment = i === breakPoints.length - 1;
 
     console.log('"' + word + '"');
 
-    let penaltyClass: PenaltyClasses;
+    let penalty: number;
     let penaltyDependsOnDistanceToNextGoodBreakingPoint;
 
     const lastLetter = word.slice(-1);
     const lastLetterClass = getLineBreakingClassOfLetterAt(input, breakPoint.position - 1);
+    const nextLetterClass = getLineBreakingClassOfLetterAt(input, breakPoint.position);
 
     lastLetterClass;
 
-    if (breakPoint.required) {
-      penaltyClass = PenaltyClasses.MandatoryBreak;
+    if (breakPoint.required || isLastSegment) {
+      penalty = PenaltyClasses.MandatoryBreak;
     }
 
     // Breaking spaces and tabs
     else if (
+      // Space
       lastLetterClass === UnicodeLineBreakingClasses.Space ||
-      (UnicodeLineBreakingClasses.BreakAfter &&
-        (lastLetter === '\t' || lastLetter.match(/\p{General_Category=Zs}/gu)))
+      // Tab
+      lastLetter === '\t' ||
+      // Other breaking spaces
+      (UnicodeLineBreakingClasses.BreakAfter && lastLetter.match(/\p{General_Category=Zs}/gu)) ||
+      // Zero width space
+      lastLetterClass === UnicodeLineBreakingClasses.ZeroWidthSpace
     ) {
-      penaltyClass = PenaltyClasses.GoodBreak;
+      penalty = PenaltyClasses.VeryGoodBreak;
+    }
+
+    // Em dash
+    else if (
+      lastLetterClass === UnicodeLineBreakingClasses.BreakOnEitherSide ||
+      nextLetterClass === UnicodeLineBreakingClasses.BreakOnEitherSide
+    ) {
+      penalty = PenaltyClasses.GoodBreak;
+    }
+
+    // Ideographic
+    else if (lastLetterClass === UnicodeLineBreakingClasses.Ideographic) {
+      penalty = PenaltyClasses.GoodBreak;
+    }
+
+    // Hyphens
+    else if (lastLetterClass === UnicodeLineBreakingClasses.Hyphen) {
+      penalty = PenaltyClasses.OKBreak;
+    }
+
+    // En-dashes and language-specific visible breaking characters
+    else if (lastLetterClass === UnicodeLineBreakingClasses.BreakAfter) {
+      penalty = PenaltyClasses.OKBreak;
+    }
+
+    // Break-before class (rare)
+    else if (nextLetterClass === UnicodeLineBreakingClasses.BreakBefore) {
+      penalty = PenaltyClasses.OKBreak;
     }
 
     // Soft hyphens
     else if (lastLetter === SOFT_HYPHEN) {
-      penaltyClass = PenaltyClasses.BadBreak;
+      penalty = PenaltyClasses.BadBreak;
     }
 
-    //
-    else if (lastLetterClass === UnicodeLineBreakingClasses.BreakAfter) {
-      penalty = 0;
-    } else if (lastLetterClass === UnicodeLineBreakingClasses.BreakAfter) {
-      penalty = 0;
+    // Slashes
+    else if (lastLetterClass === UnicodeLineBreakingClasses.SymbolAllowingBreakAfter) {
+      penalty = PenaltyClasses.VeryBadBreak;
     }
 
-    // const nextLetterClass = getLineBreakingClassOfLetterAt(input, breakPoint.position);
-    // nextLetterClass;
+    // Other break-classes
+    else {
+      penalty = PenaltyClasses.VeryBadBreak;
+    }
   }
 
   /* tmp test */
@@ -102,7 +140,7 @@ export const getLineBreakingClassOfLetterAt = (
   ] as UnicodeLineBreakingClasses;
 };
 
-getBreakablePoints('b-1a');
+getBreakablePoints('b–a');
 
 /**
  * "The recommendation in this case is for the layout system not to utilize a
