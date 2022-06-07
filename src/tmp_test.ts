bda508cfe89f12a36bb3e8502953aaaa62c6b84c
Egill <egill@egill.xyz>
2022-06-07 08:59:12 +0000

    no message

--- src/tmp_test.ts
+++ src/tmp_test.ts
@@ -1,10 +1,15 @@
+import enUsPatterns from "hyphenation.en-us";
+import Hypher from "hypher";
 import { texLinebreakMonospace } from "src/utils/monospace";
+const hypher = new Hypher(enUsPatterns);
 
-const text = `A very_very_very_very_very_long_string here.`;
+const text = `When the first paper volume of Donald Knuth's The Art of Computer Programming was published in 1968,[4] it was typeset using hot metal typesetting set by a Monotype Corporation typecaster. This method, dating back to the 19th century, produced a "good classic style" appreciated by Knuth.`;
 const t = texLinebreakMonospace(text, {
-  lineWidth: 20,
-  forceOverflowToBreak: false,
+  lineWidth: 31,
+  infiniteGlueStretchAsRatioOfWidth: 0.5,
+  softHyphenPenalty: 10,
+  hyphenateFn: (x: string) => hypher.hyphenate(x),
 });
 
-// console.log(t.items.slice(-4));
+console.log(t.items.slice(-4));
 console.log(t.plainTextLines);
