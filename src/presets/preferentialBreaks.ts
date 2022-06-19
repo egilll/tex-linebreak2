79650986de0b56de56c380ca0f88ba21fbad70ff
Egill <egill@egill.xyz>
2022-06-19 14:00:00 +0000

    wip

---
+++ src/presets/preferentialBreaks.ts
@@ -0,0 +1,8 @@
+/**
+ * To add:
+ *
+ * - Preferentially break after `."`, `[a-z]{5,}\.` (long words followed
+ *   by period)
+ * - Try not to break after "Mr.", "Mrs.", "Dr.", "Prof.", "Sir", "Rev.",
+ * - Try not to break before "Jr.", "Sr.", "Ph.D."
+ */
